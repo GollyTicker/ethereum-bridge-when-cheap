c0ec3a6ad84ddf698b11413cf78b9576e3881891
GollyTicker <golly.ticker@gmail.com>
2023-04-06 23:58:46 +0200

    backend wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,25 @@
-'use strict';
+"use strict";
 
-import { Logger } from 'sitka';
-import Express from 'express';
-import { Hop } from '@hop-protocol/sdk';
+import { Logger } from "sitka";
+import Express from "express";
+import { Hop } from "@hop-protocol/sdk";
 
-const logger: Logger = Logger.getLogger('index');
+const logger: Logger = Logger.getLogger("index");
 
 const app = Express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 
-	const hop = new Hop('mainnet', undefined, undefined);
-	logger.info('Hop', hop);
+  const hop = new Hop("arbitrum", undefined, undefined);
+  logger.info("Hop", hop);
+
+  const daiBridge = hop.bridge("DAI");
+  logger.info("Dai bridge", daiBridge);
+  daiBridge.getSendData();
 });
 
 app.listen(port, () => {
-	logger.info(`Example app listening on port ${port}`);
+  logger.info(`Example app listening on port ${port}`);
 });
