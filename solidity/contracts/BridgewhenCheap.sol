f5cd947c10e8138f1eb6b3c981092fc89b49c520
GollyTicker <golly.ticker@gmail.com>
2023-04-16 20:21:07 +0200

    prepare calling of l2 amm smart contract

--- solidity/contracts/BridgewhenCheap.sol
+++ solidity/contracts/BridgewhenCheap.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -7,38 +8,42 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 struct BridgeRequest {
     address source;
     address destination;
-    uint amount;
-    uint wantedL1GasPrice;
-    // todo. need more arguments here to be able to call hop's L2_AMMWrapper's swapAndSend
+    uint256 amount;
+    uint256 amountOutMin; // this is calculated in the UI and shown to the user as a guarantee of what will definitely arrive.
+    // tod. we need to decrease this by our estimated amount of what changes due differing situations between submit-time and execution time.
+    uint256 wantedL1GasPrice;
     // todo. support tokens and not only native ether
-    // todo. Ideally, we want to allow a specific minAmountOut, so that the users funds are not being eaten away.
-    //       But for that, we'll need some satistics on how much lower the actual bonder fee is when swaps are actually executed.
 }
 
 /// @custom:security-contact bridge-when-cheap@gmail.com
 contract BridgeWhenCheap is Ownable, ReentrancyGuard {
     // The amount of total gas required to execute a single L2 -> L1 Hop Bridge.
     // This amount is deducted from requestors to pay for the gas fees of the delayed execution.
-    uint executionGasRequirement;
+    uint256 executionGasRequirement;
 
     // address of the deloyed hop bridge to interact with to bridge funds.
-    address l2HopBridgeAmmWrapper;
+    L2_AmmWrapper public l2HopBridgeAmmWrapper;
     // goerli arbitrum: 0xa832293f2DCe2f092182F17dd873ae06AD5fDbaF
 
     // For each requestor, it stores a struct with the request details for the briding.
     mapping(address => BridgeRequest) requests;
+
     // todo. make it possible to have multiple requests per address.
 
     constructor(
-        uint _executionGasRequirement, /* 0.00015 ether recommended*/
-        address _l2HopBridgeAmmWrapper
+        uint256 _executionGasRequirement, /* 0.00015 ether recommended*/
+        L2_AmmWrapper _l2HopBridgeAmmWrapper
     ) {
         executionGasRequirement = _executionGasRequirement;
         l2HopBridgeAmmWrapper = _l2HopBridgeAmmWrapper;
     }
 
     // View any pending requests belonging to the caller
-    function pendingRequests() public view returns (BridgeRequest memory, bool) {
+    function pendingRequests()
+        public
+        view
+        returns (BridgeRequest memory, bool)
+    {
         BridgeRequest memory request = requests[msg.sender]; // Copy, not reference.
         return (request, isDefined(request));
     }
@@ -47,19 +52,37 @@ contract BridgeWhenCheap is Ownable, ReentrancyGuard {
     // when the L1 gas fees are at wantedL1GasPrice or lower.
     // The request is recorded in the smart contract and executed lateron,
     // by the owner of the contract, when the gas fees are low.
-    function deposit(address destination, uint wantedL1GasPrice) public payable {
+    function deposit(
+        address destination,
+        uint256 wantedL1GasPrice,
+        uint256 amountOutMin
+    ) public payable {
         require(msg.sender != address(0), "Sender may not be 0 address");
-        require(destination != address(0), "Destination address may not be 0 address");
-        require(wantedL1GasPrice > 0, "Wanted L1 gas price must be strictly positive");
-        require(msg.value >= executionGasRequirement, "Not enough funds to pay for delayed execution");
-        
-        uint sentAmount = msg.value - executionGasRequirement; // keep some funds for ourselves for execution + service fee
-        // payable(this.address).send(executionGasRequirement); // we don't need to actually send the funds to ourselves, right?
+        require(
+            destination != address(0),
+            "Destination address may not be 0 address"
+        );
+        require(
+            wantedL1GasPrice > 0,
+            "Wanted L1 gas price must be strictly positive"
+        );
+        require(
+            msg.value >= executionGasRequirement,
+            "Not enough funds to pay for delayed execution"
+        );
+
+        uint256 sentAmount = msg.value - executionGasRequirement; // keep some funds for ourselves for execution + service fee
+
+        require(
+            sentAmount >= amountOutMin,
+            "Calculated sent amount must be larger than the required minimum amount arriving at destination."
+        );
 
         requests[msg.sender] = BridgeRequest({
             source: msg.sender,
             destination: destination,
             amount: sentAmount,
+            amountOutMin: amountOutMin,
             wantedL1GasPrice: wantedL1GasPrice
         });
     }
@@ -78,12 +101,23 @@ contract BridgeWhenCheap is Ownable, ReentrancyGuard {
         require(success, "Failed to withdraw funds to msg.sender");
     }
 
-    // Execute the request for the given requestor address. 
+    // Execute the request for the given requestor address.
     // The execution gas is refunded to the caller and the bridging
     // is executed via the Hop Bridge.
-    function executeRequest(address requestor) public onlyOwner nonReentrant {
+    function executeRequest(
+        address requestor,
+        /* these fields are calculated just before executing the request to find these parameters via "populateSendTx" */
+        uint256 bonderFee,
+        SwapData memory swapData,
+        SwapData memory destinationSwapData,
+        address bonder
+    ) public onlyOwner nonReentrant {
         BridgeRequest memory toBeBridgedRequest = requests[requestor];
         require(isDefined(toBeBridgedRequest), "No request to process");
+        require(
+            toBeBridgedRequest.amount >= bonderFee,
+            "Bonder fee cannot exceed amount."
+        );
 
         delete requests[requestor];
 
@@ -91,19 +125,46 @@ contract BridgeWhenCheap is Ownable, ReentrancyGuard {
         bool success = payable(msg.sender).send(executionGasRequirement);
         require(success, "Failed to refund executor");
 
-        // todo. we need to test the sending here as well.
-        // todo. actually use request arguments
-        success = payable(l2HopBridgeAmmWrapper).send(toBeBridgedRequest.amount);
-        require(success, "Failed to submit bridge request to Hop Bridge");
+        l2HopBridgeAmmWrapper.swapAndSend(
+            1, // destination always mainnet
+            toBeBridgedRequest.destination,
+            toBeBridgedRequest.amount,
+            bonderFee,
+            swapData,
+            destinationSwapData,
+            bonder
+        );
     }
 
     // If the L2 network gas prices rise for a longer duration, we can adapt the gas deposit the users have to make.
-    function setExecutionGasRequirement(uint amount) public onlyOwner {
+    function setExecutionGasRequirement(uint256 amount) public onlyOwner {
         executionGasRequirement = amount;
     }
 
     // Returns true iff the request is not it's default zero-value.
-    function isDefined(BridgeRequest memory request) pure internal returns (bool) {
+    function isDefined(BridgeRequest memory request)
+        internal
+        pure
+        returns (bool)
+    {
         return request.source != address(0);
     }
+}
+
+interface L2_AmmWrapper {
+    function swapAndSend(
+        uint256 chainId,
+        address recipient,
+        uint256 amount,
+        uint256 bonderFee,
+        SwapData memory swapData,
+        SwapData memory destinationSwapData,
+        address bonder
+    ) external payable;
+}
+
+struct SwapData {
+    uint8 tokenIndex;
+    uint256 amountOutMin;
+    uint256 deadline;
 }
