8f7f02dd59b82c1b20f9980459dd4341f3f4b5d9
GollyTicker <golly.ticker@gmail.com>
2023-05-17 20:19:54 +0000

    Mandatory activate swapfile

--- server/start-main.sh
+++ server/start-main.sh
@@ -4,6 +4,8 @@ set -eu
 
 cd server 2>/dev/null || true
 
+sudo swapon /swapfile || true
+
 if [[ "$(swapon -s)" == "" ]]; then
   echo "OS must use Swap of at least 1g. Otherwise OS will get OOM issues!"
   echo "Check: https://linuxize.com/post/create-a-linux-swap-file/"
